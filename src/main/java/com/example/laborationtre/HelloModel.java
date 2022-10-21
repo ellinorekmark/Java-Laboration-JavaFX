9ad75819ccdb589b962a9852e1b7610b686fb5c6
Ellinor <ellinorekmark@hotmail.com>
2022-10-21 18:11:55 +0200

    It's a start...

---
+++ src/main/java/com/example/laborationtre/HelloModel.java
@@ -0,0 +1,4 @@
+package com.example.laborationtre;
+
+public class HelloModel {
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.laborationtre {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.laborationtre to javafx.fxml;
+    exports com.example.laborationtre;
+}
