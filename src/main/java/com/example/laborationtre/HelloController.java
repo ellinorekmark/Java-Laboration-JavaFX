0e6c24f1e0df934dea407a0db34659f8fd3baf3b
Ellinor <ellinorekmark@hotmail.com>
2022-10-24 11:03:44 +0200

    quality of life changes, tools are preset, size is now a slider

--- src/main/java/com/example/laborationtre/HelloController.java
+++ src/main/java/com/example/laborationtre/HelloController.java
@@ -1,14 +1,13 @@
 package com.example.laborationtre;
 
 import javafx.event.EventHandler;
-import javafx.event.EventType;
+
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.ColorPicker;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.StrokeLineCap;
 
 
@@ -22,7 +21,7 @@ public class HelloController {
 
     public static GraphicsContext context;
     public ChoiceBox<String> toolsList;
-    public ChoiceBox<String> pixelSize;
+
 
     public EventHandler<MouseEvent> onLinesMousePressedEvent;
 
@@ -30,46 +29,56 @@ public class HelloController {
 
     public EventHandler<MouseEvent> onMouseDraggedEvent;
 
+    public Slider pixelSlider;
+    public Label pixelSizeInfo;
+
 
     public void initialize() {
         context = canvas.getGraphicsContext2D();
+        //sets some choices in the tools options etc. to start with
+        colorChoice.setValue(Color.BLACK);
+        pixelSizeInfo.setText(Math.round(pixelSlider.getValue()) + " px");
 
-        onLinesMousePressedEvent = mouseEvent -> {
-            context.setStroke(colorChoice.getValue());
-            context.setLineWidth(Double.parseDouble(pixelSize.getValue()));
-            context.setLineCap(StrokeLineCap.ROUND);
-            context.beginPath();
+        toolsList.setValue("Free Draw");
 
-            context.moveTo(mouseEvent.getX(), mouseEvent.getY());
+
+        onLinesMousePressedEvent = (MouseEvent mouseEvent) -> {
+            if(toolsList.getValue().equalsIgnoreCase("Stamp - Square")){
+                context.setFill(colorChoice.getValue());
+                context.fillRect(mouseEvent.getX()-(pixelSlider.getValue()/2), mouseEvent.getY()-(pixelSlider.getValue()/2), pixelSlider.getValue(), pixelSlider.getValue());
+            }
+            else {
+
+                context.setStroke(colorChoice.getValue());
+                context.setLineWidth(pixelSlider.getValue());
+                context.setLineCap(StrokeLineCap.ROUND);
+                context.beginPath();
+
+                context.moveTo(mouseEvent.getX(), mouseEvent.getY());
+                context.stroke();
+            }
+
+        };
+        onLinesMouseReleasedEvent = mouseEvent -> {
+            context.lineTo(mouseEvent.getX(), mouseEvent.getY());
             context.stroke();
-
+            context.closePath();
         };
-        onLinesMouseReleasedEvent = new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                context.lineTo(mouseEvent.getX(), mouseEvent.getY());
-                context.stroke();
-                context.closePath();
-                //do stuff.
-            }
-        };
-        onMouseDraggedEvent = new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                context.lineTo(mouseEvent.getX(), mouseEvent.getY());
-                context.stroke();
-                //do stuff.
-            }
+        onMouseDraggedEvent = mouseEvent -> {
+            context.lineTo(mouseEvent.getX(), mouseEvent.getY());
+            context.stroke();
         };
 
-
-
+    }
+    @FXML
+    protected void setNewPixelSize(){
+        pixelSizeInfo.setText(Math.round(pixelSlider.getValue()) + " px");
     }
 
 
 
     @FXML
-    protected void onCanvasPress(MouseEvent mouseEvent) {
+    protected void onCanvasPress() {
 
 
         if (toolsList.getValue().equalsIgnoreCase("lines")) {
@@ -83,6 +92,12 @@ public class HelloController {
             canvas.addEventHandler(MouseEvent.MOUSE_PRESSED, onLinesMousePressedEvent);
             canvas.removeEventHandler(MouseEvent.MOUSE_RELEASED, onLinesMouseReleasedEvent);
         }
+        else if (toolsList.getValue().equalsIgnoreCase("Stamp - Square")) {
+            canvas.removeEventHandler(MouseEvent.MOUSE_DRAGGED, onMouseDraggedEvent);
+            canvas.addEventHandler(MouseEvent.MOUSE_PRESSED, onLinesMousePressedEvent);
+            canvas.removeEventHandler(MouseEvent.MOUSE_RELEASED, onLinesMouseReleasedEvent);
+        }
+
     }
 
 }
